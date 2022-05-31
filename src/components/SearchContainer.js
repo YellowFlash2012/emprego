d4614383cc2cb6007806ce57fbbf637b1d6b2237
YellowFlash2012 <cyclop9@hotmail.com>
2022-05-31 08:40:06 +0100

    search container ui completed

--- src/components/SearchContainer.js
+++ src/components/SearchContainer.js
@@ -1,4 +1,65 @@
+import { useDispatch, useSelector } from "react-redux";
+import Wrapper from "../helpers/SearchContainerSC";
+import FormRow from "./FormRow";
+import FormRowSelect from "./FormRowSelect";
+
 const SearchContainer = () => {
-    return <div>searchContainer</div>;
+    const { isLoading, search, searchStatus, searchType, sort, sortOptions } = useSelector(store => store.allJobs);
+
+    const { jobTypeOptions, statusOptions } = useSelector(store => store.job);
+
+    const dispatch = useDispatch();
+
+    const handleSearch = (e) => { }
+
+    const clearFiltersHandler = (e) => {
+        e.preventDefault()
+    }
+    
+    return (
+        <Wrapper>
+            <form className="form">
+                <h4>search form</h4>
+                <div className="form-center">
+                    {/* plain search */}
+                    <FormRow
+                        type="text"
+                        name="search"
+                        value={search}
+                        handleChange={handleSearch}
+                    />
+
+                    {/* search status */}
+                    <FormRowSelect
+                        labelText="status"
+                        name="searchStatus"
+                        value={searchStatus}
+                        handleChange={handleSearch}
+                        list={["all", ...statusOptions]}
+                    />
+
+                    {/* search by type*/}
+                    <FormRowSelect
+                        labelText="type"
+                        name="searchType"
+                        value={searchType}
+                        handleChange={handleSearch}
+                        list={["all", ...jobTypeOptions]}
+                    />
+                    {/* sort */}
+                    <FormRowSelect
+                        name="sort"
+                        value={sort}
+                        handleChange={handleSearch}
+                        list={sortOptions}
+                    />
+
+                    <button className="btn btn-block btn-danger" onClick={clearFiltersHandler}>
+                        clear
+                    </button>
+                </div>
+            </form>
+        </Wrapper>
+    );
 };
 export default SearchContainer;
